61b7b19844a1da609b887d12fc8ff3ad3b095eff
Trung Nguyen <nguyenchitrung1310@gmail.com>
2020-12-12 14:09:39 +0700

    Delte

--- e-learning/.stylelintrc.js
+++
@@ -1,6 +0,0 @@
-const fabric = require('@umijs/fabric');
-fabric.stylelint.rules['order/properties-order'] = false;
-module.exports = {
-  // ...fabric.stylelint,
-  rules: {},
-};
--- e-learning/src/.umi/core/plugin.ts
+++
@@ -1,8 +0,0 @@
-// @ts-nocheck
-import { Plugin } from '/Volumes/MR TRUNG/ReactJS/Project/Course-react-antd/e-learning/node_modules/@umijs/preset-built-in/node_modules/@umijs/runtime';
-
-const plugin = new Plugin({
-  validKeys: ['modifyClientRenderOpts','patchRoutes','rootContainer','render','onRouteChange',],
-});
-
-export { plugin };
